40971f64c110310c7fb4cda349d4ad5813142bb3
vhgtvictor <victor01.teles@gmail.com>
2026-05-22 13:22:51 -0300

    fix : schema_prisma

--- backend/src/lib/prisma.js
+++ backend/src/lib/prisma.js
@@ -1,4 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasourceUrl: process.env.DATABASE_URL,
+});
 export default prisma;
